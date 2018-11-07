514ef1de392abfc331c2e1803855bfe6b5d92127
ec2-user <hiroaki.gushiken@casleyconsulting.co.jp>
2018-11-07 23:01:42 +0000

    テストコードが通るように修正

--- test_sample.py
+++ test_sample.py
@@ -6,3 +6,11 @@ class TestSample(unittest.TestCase):
     def test_fizzbuzz(self):
         result = sample.fizzbuzz(15)
         self.assertEqual(result,'fizzbuzz')
+
+    def test_fizzbuzz2(self):
+        result = samplefizzbuzz(5)
+        self.assertEqual(result,'cas')
+
+    def test_fizzbuzz3(self):
+        result = samplefizzbuzz(4)
+        self.assertEqual(result,'cas')
